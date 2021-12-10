3a7bb790dbf3d3e90392adbaf833a136690d3c8c
Valeriia-Ro <vkolchak17@gmail.com>
2021-12-10 12:33:50 +0200

    done

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,6 +1,5 @@
 const validation = (schema) => {
   return async (req, res, next) => {
-    console.log(req.body);
     const { error } = schema.validate(req.body);
     if (error) {
       res.status(400).json({
@@ -13,18 +12,4 @@ const validation = (schema) => {
     next();
   };
 };
-
 module.exports = validation;
-
-// let x = await (async function () {
-//   return "hello";
-// })();
-// console.log(x);
-// // or
-// console.log(await (async () => "hello")());
-
-// async function hello(){
-//   return "hello";
-// }
-// let x = await hello();
-// console.log(x);
